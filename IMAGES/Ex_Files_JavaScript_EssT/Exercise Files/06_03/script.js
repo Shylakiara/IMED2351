0438d1b8cd87f6547b3cd606130e5ea106690839
Shylakiara <57325834+Shylakiara@users.noreply.github.com>
2020-11-12 15:47:03 -0600

    clock

---
+++ IMAGES/Ex_Files_JavaScript_EssT/Exercise Files/06_03/script.js
@@ -0,0 +1,11 @@
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+let hrPostion = 20;
+let minPostion = 130;
+let secPostion = 267;
+
+HOURHAND.style.transform = "rotate(" + hrpostion + "deg)";
+MINTUTEHAND.style.transform = "rotate(" + minpostion + "deg)";
+SECONDHAND.style.transform = "rotate(" + secpostion + "deg)";
